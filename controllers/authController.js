8b81b3dda1ad9709615b10aafadcce0bd367df64
HammadAli01 <61947146+HammadAli01@users.noreply.github.com>
2020-06-16 17:09:12 +0500

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const crypto = require('crypto');
+const crypto = require('crypto'); 
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
