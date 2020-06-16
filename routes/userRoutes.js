2ff7b88ae80265fb6870319bd0273f2dffa5ab8e
HammadAli01 <61947146+HammadAli01@users.noreply.github.com>
2020-06-16 17:15:11 +0500

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,7 +11,7 @@ router.get('/logout', authController.logout);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
-// Protect all routes after this middleware
+// Protect all route after this middleware
 router.use(authController.protect);
 
 router.patch('/updateMyPassword', authController.updatePassword);
